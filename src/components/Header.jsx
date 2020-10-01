8d4cc2c780380f5f812082b18664951ebd2b8a1a
framirezgarrido1 <f.ramirez@globant.com>
2020-10-01 15:32:58 -0300

    .gitignore is now working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import '../css/App.css';
 
 // Material UI Components
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton/IconButton';
+import { MenuIcon, AccountCircle } from '@material-ui/icons';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
 
 function Header() {
+    
+const classes = useStyles();
+
   return (
       <header>
         <AppBar position="static">
-            <Toolbar variant="dense">
-            <IconButton edge="start" color="inherit" aria-label="menu">
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit">
-                Dashboard
-            </Typography>
-            </Toolbar>
-        </AppBar>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <AccountCircle />
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       </header>
   );
 }
